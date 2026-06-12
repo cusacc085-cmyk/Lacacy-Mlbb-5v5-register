097b2d2a88473d1a48b28533dfd44b6ff40fb2f3
cusacc085-cmyk <cusacc085@gmail.com>
2026-06-12 14:53:03 +0530

    Create teams.js

---
+++ api/teams.js
@@ -0,0 +1,41 @@
+const clientPromise = require("../lib/mongodb");
+
+module.exports = async (req, res) => {
+
+```
+if (req.method !== "GET") {
+    return res.status(405).json({
+        success: false,
+        message: "Method Not Allowed"
+    });
+}
+
+try {
+
+    const client = await clientPromise;
+
+    const db = client.db("SlimeBot");
+
+    const collection =
+        db.collection("mlbb_teams");
+
+    const teams = await collection
+        .find({})
+        .sort({ createdAt: 1 })
+        .toArray();
+
+    res.status(200).json(teams);
+
+} catch (error) {
+
+    console.error(error);
+
+    res.status(500).json({
+        success: false,
+        message: "Internal Server Error"
+    });
+
+}
+```
+
+};
